4ac9c6ad18aea5a8550470ce53f2c4f20c9d5714
Sebastian Sarasti <82113558+sebassaras02@users.noreply.github.com>
2022-01-25 11:48:53 -0500

    Delete build directory

--- build/CMakeFiles/NaInew.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/NaInew"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/NaInew.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
