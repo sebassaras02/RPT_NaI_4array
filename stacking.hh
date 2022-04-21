f32cf8ff4b8e6fa5c4b3731d807fc689257f376b
Sebastian Sarasti <sebitas.alejo@hotmail.com>
2022-04-20 19:42:37 -0500

    Deleting stacking action messenger, test failed

--- stacking.hh
+++ stacking.hh
@@ -6,10 +6,6 @@
 #include "G4Track.hh"
 #include "G4NeutrinoE.hh"
 #include "G4Neutron.hh"
-#include "stackingmessenger.hh"
-
-
-class stackingmessenger;
 
 
 class stackingaction : public G4UserStackingAction
@@ -19,16 +15,6 @@ class stackingaction : public G4UserStackingAction
     ~stackingaction();
 
     G4ClassificationOfNewTrack ClassifyNewTrack(const G4Track*);
-
-    void SetKillNeutrino(G4bool a);
-    void SetKillNeutron(G4bool a);
-    void SetDefaultValues();
-
-  private:
-    G4bool killnuetrinofun;
-    G4bool killnuetronfun;
-
-    stackingmessenger *fStackmessenger;
 };
 
 
