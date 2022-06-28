105924b65246f5bcedd07d9991c33f996816f2e0
Sebastian Sarasti <sebitas.alejo@hotmail.com>
2022-06-28 12:05:14 -0500

    Solving line.sh 2

--- lines_rpt.sh
+++ lines_rpt.sh
@@ -3,15 +3,15 @@ start_time=$(date "+%s")
 # comment
 
 for i in {1..200}
-for j in {1..200}
 do 
-do 
-    ./naidetector /G4-e/RPT_NaI_4array/matrix/macros/traje$ipos$j.mac
-    root -q det_reader1.C
-    mv NaIDetector_matrix.dat NaIDetector_matrix_traje$ipos$j.dat
-    mv NaIDetector_matrix_traje$ipos$j.dat /G4-e/results_RPT_ML/matrix/NaIDetector_matrix_traje$ipos$j.dat
-    rm *.root
-done
+	for j in {1..200}
+	do 
+	    ./naidetector /G4-e/RPT_NaI_4array/matrix/macros/traje$ipos$j.mac
+	    root -q det_reader1.C
+	    mv NaIDetector_matrix.dat NaIDetector_matrix_traje$ipos$j.dat
+	    mv NaIDetector_matrix_traje$ipos$j.dat /G4-e/results_RPT_ML/matrix/NaIDetector_matrix_traje$ipos$j.dat
+	    rm *.root
+	done
 done
 
 end_time=$(date "+%s")
