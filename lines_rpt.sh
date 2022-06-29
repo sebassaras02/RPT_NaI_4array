fcc7c162426f6797619efa169cde12c8d5f40ef9
Sebastian Sarasti <sebitas.alejo@hotmail.com>
2022-06-29 10:39:05 -0500

    Solving lines.sh

--- lines_rpt.sh
+++ lines_rpt.sh
@@ -6,10 +6,10 @@ for i in {1..200}
 do 
 	for j in {1..200}
 	do 
-	    ./naidetector /G4-e/RPT_NaI_4array/matrix/macros/traje$ipos$j.mac
+	    ./naidetector /G4-e/RPT_NaI_4array/matrix/macros/traje${i}pos${j}.mac
 	    root -q det_reader1.C
-	    mv NaIDetector_matrix.dat NaIDetector_matrix_traje$ipos$j.dat
-	    mv NaIDetector_matrix_traje$ipos$j.dat /G4-e/results_RPT_ML/matrix/NaIDetector_matrix_traje$ipos$j.dat
+	    mv NaIDetector_matrix.dat NaIDetector_matrix_traje${i}pos${j}.dat
+	    mv NaIDetector_matrix_traje${i}pos${j}.dat /G4-e/results_RPT_ML/matrix/NaIDetector_matrix_traje${i}pos${j}.dat
 	    rm *.root
 	done
 done
